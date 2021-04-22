2d58462fc3b2f15466459564d1c760fb7997841d
aman tiwari <amantiwari78632@gmail.com>
2021-04-23 01:52:56 +0530

    fix not auto scroll after route

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-
+import ScrollToTop from "./website/utils/ScrollToTop";
 import App from './website/App';
 import store from './shared/store';
 ReactDOM.render(
@@ -10,6 +10,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store} >
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
